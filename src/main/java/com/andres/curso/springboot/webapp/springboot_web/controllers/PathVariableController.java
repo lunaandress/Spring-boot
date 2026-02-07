0a6a14b33f0d6d6aeae64a20b4953cff7d531d7b
Andres Luna <miosesfreddy123@gmail.com>
2026-02-07 14:30:57 +0100

    Obtener varios parámetros de la ruta con @PathVariable

--- src/main/java/com/andres/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/andres/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,11 +1,15 @@
 package com.andres.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andres.curso.springboot.webapp.springboot_web.models.ParamDto;
+
 
 
 @RestController
@@ -25,4 +29,23 @@ public ParamDto baz(@PathVariable String mensaje) {
     
 
 
+@GetMapping("/mix/{product}/{id}")
+public Map<String, Object> getMethodName(@PathVariable String product, @PathVariable long id) {
+
+    Map<String, Object > json  = new HashMap<>();
+    json.put("product", product);
+    json.put("id", id);
+
+    return json;
 }
+
+//http://localhost:8080/api/var/mix/telefono/003
+
+
+}
+
+
+
+
+
+
